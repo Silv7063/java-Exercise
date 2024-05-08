702553f1beb457ce7b6c24e3f43dcf64e1abe486
Henry1314520 <ongyeetorng17@gmail.com>
2024-05-08 21:58:32 +0800

    adding name and id
    adding 學號01157065 + 姓名：王昱童

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team{
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157002\"\n姓名：\"宋碩瑋\"");
+        System.out.println("\"學號：\"01157065\"\n姓名：\"\"王昱童");
     }
 }
