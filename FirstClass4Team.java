24d6681c9753f9a3709280521375ada60feae79d
hkchukk <42998246+hkchukk@users.noreply.github.com>
2024-05-08 22:06:14 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team{
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157002\"\n姓名：\"宋碩瑋\"");
         System.out.println("\"學號：\"01157065\"\n姓名：\"\"王昱童");
+        System.out.println("\"學號：\"01157165\"\n姓名：\"朱建強\"");
     }
 }
